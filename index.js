21e682ff45f94da9e6d843b6937f291ca14446c0
cvaize <cvaize@gmail.com>
2020-10-06 03:24:55 +0500

    UMD export

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-export default function thlinkify(inputText) {
-    let replacedText, replacePattern1, replacePattern2, replacePattern3;
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+}(this, function () {
+    return function (inputText) {
+        var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-ZА-ЯЁ0-9+&@#\/%?=~_|!:,.;]*[-A-ZА-ЯЁ0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-ZА-ЯЁ0-9+&@#\/%?=~_|!:,.;]*[-A-ZА-ЯЁ0-9+&@#\/%=~_|])/gim;
+        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
-    //Russian characters in links
-    replacedText = replacedText.replace(/href="(.*?)"/gim, function (value){
-        return 'href="'+encodeURI(value.replace(/href="/gim, '').replace(/"/gim, ''))+'"';
-    });
+        //Russian characters in links
+        replacedText = replacedText.replace(/href="(.*?)"/gim, function (value){
+            return 'href="'+encodeURI(value.replace(/href="/gim, '').replace(/"/gim, ''))+'"';
+        });
 
-    return replacedText;
-}
+        return replacedText;
+    }
+}));
